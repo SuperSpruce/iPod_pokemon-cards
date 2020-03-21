88097b42ae0283d7d94a966608563b28addab51b
SuperSpruce <48099460+SuperSpruce@users.noreply.github.com>
2020-03-20 21:40:48 -0400

    Update 2.4

--- Card.java
+++ Card.java
@@ -21,7 +21,27 @@ public class Card
     "Back on Track", "Measures of Success 1.6", "Best of Wives and Best of Women", "Pokemon G1 Viridian Forest", "Pokemon G3' Inside Gym", "Pokemon G1 Mt. Moon", "Metal Gear Alert", "Measures of Success 1.19", "Measures of Success 3.8",
     "Polargeist", "Paperback Writer", "Pokemon G3' Route 110", "Measures of Success 2.9", "I am the Walrus", "Pokemon G1 Cinnabar Island", "Pokemon G6 Legendary", "Clash of Clans Clan War", "Let It Go (A-flat)",
     
-    "Track 21"
+    "Track 21", "DNA", "Goldberg Variation 1", "Clicker Heroes 2: Zone 100", "Pokemon G1 Evolution", "Pokemon G3' Team Magma and Aqua 2", "Hellcat", "Pokemon G7 Trainer", "Pokemon G5 Team Plasma 3: Ghetsis",
+    "Measures of Success 1.4", "Pokemon G1 Wild", "Pokemon G1' Trainer", "Fixer Upper", "Bomba But It's Bass Boosted Attempt 2", "Santa's Beard", "Measures of Success 2.1", "Measures of Success Introduction", "xStep",
+    "Pokemon G5 Champion", "Pokemon G1 Capture Pokemon", "Cabinet Battle 1", "Clash of Clans All Builder Base Soundtracks", "Bemsha Swing", "D: K247", "Pokemon G2' Route 38", "Pokemon G3 Gym Battle", "D: K491",
+    "We Know", "Track 14", "Goldberg Variation 26", "King Taco", "Investigations", "Measures of Success 1.9", "Pokemon G5' Wild", "Mozart Opera Impero", "Pokemon G2 Team Rocket Battle",
+    "I want to hold your hand", "More than meets the eye", "Nobody Quite as Hip", "Pokemon G5 Wild", "Measures of Success 2.17", "The Elements", "Piece of Dirt", "Number Two", "4 4's True Cry",
+    "Star Wars Imperial March Trap Remix", "Cowtown", "Track 10", "Pokemon G3' Wild", "D: HWV 430.2", "Hey Jude", "Pokemon G2' Hall of Fame", "War Game", "Goldberg Variation 29",
+    "Pokemon G1'' Wild", "Cable Kid", "Pokemon G3' Champion", "Measures of Success 1.20", "They'll need a crane", "D: HWV428.5", "Measures of Success 1.5", "Snuggle Puppy", "Straight, No Chaser",
+    "Vivaldi Summer I", "Goldberg Variation Aria De Capo", "Measures of Success 2.22", "Measures of Success Interpretation Station 1", "At the Speed of Light", "Pokemon G1 Encounter Trainer 1", "Boker Tov", "Fly Away", "Ruby, my Dear",
+    "The Price is Right Losing Horn", "Goldberg Varation 30", "PACER TEST", "Snowball in Hell", "Pokemon G7 Totem", "Track 13", "Wake Up Call", "D: K537", "Vivaldi Winter I",
+    
+    "In C", "Measures of Success 2.31", "Mozart Opera Credi", "Measures of Success 1.29", "Pokemon G2' Route 26", "Measures of Success 2.19", "Pokemon G4 Lengedary 4: Arceus", "Pokemon G1 Celadon City", "Mario Power Down Sound Effect",
+    "Theory of Everything 2", "Beethoven Spring I", "Measures of Success 2.7", "The Reynolds Pamphlet", "It's Over 9000!!! [short]", "Can't Let Go", "I Don't Like", "Mozart Opera Avede", "Goldberg Variation 15",
+    "Pokemon G2 R", "The Schuyler Sisters", "Different Trains III: After the War", "Pokemon G4 Champion", "Beethoven Kreutzer I", "Beethoven Kreutzer II", "Guns and Ships", "Love is an Open Door", "Your Obediant Servant",
+    "Clash Royale Epic Card", "Pokemon G1 Encounter Trainer 2", "Yay Sound Effect", "Clash Royale Legendary Card New", "D: HWV427.3", "Pokemon G1 Team Rocket Hideout", "Goldberg Variation 10", "Clash of Clans: Clouds", "Goldberg Variation 3",
+    "Track 25", "Don't Ask Me Why", "Electrodynamix", "You'll Miss Me", "Joshua Bell Interview", "To Be Continued Sound Effect", "Satisfied", "Washington On Your Side", "Give Me Candy",
+    "Pokemon G3 Champion", "Goldberg Variation 6", "Michael, Row the Boat Ashore", "Mozart Opera Flori", "Goldberg Variation 4", "Unity", "Measures of Success 2/24", "Beethoven G Major II", "Ten Duel Commandments",
+    "Goldberg Variation 5", "Lady Madonna", "Pokemon G3' Gym Battle", "Spazzmatica Polka", "Marvin Suggs and his Muppaphone", "Goldberg Variation 12", "All You Need is Love", "Beethoven Spring II", "Mozart Opera Pieta",
+    "Measures of Success 3.10", "Expecto Patronum", "Africa (noob)", "Pokemon G2' Title Screen", "Pokemon G1 Encounter Rival", "Infinite Power", "Mario Stage Clear", "Clash Roayle: Draw", "Measures of Success 2.15",
+    "Measures of Success 2.23", "Harry Potter Theme Song 2", "Crab Rave But It's Bass Boosted", "Goldberg Variation 20", "Xenogenesis", "History Has It's Eyes on You", "The Builder", "Measures of Success 1.18", "Measures of Success 1.24",
+    
+    "Pokemon G4 Trainer"
     };
     
     
@@ -48,7 +68,27 @@ public class Card
     "metal", "dark", "fighting", "normal", "normal", "normal", "item", "dark", "dark",
     "metal", "fire", "normal", "dark", "fire", "normal", "normal", "metal", "water",
     
-    "water"
+    "water", "unknown", "leaf", "metal", "normal", "normal", "dragon", "normal", "normal",
+    "dark", "normal", "normal", "water", "metal", "fire", "dark", "dark", "metal",
+    "normal", "item", "fighting", "metal", "unknown", "leaf", "normal", "normal", "leaf",
+    "fighting", "water", "leaf", "dragon", "electric", "dark", "normal", "leaf", "normal",
+    "fire", "unknown", "unknown", "normal", "dark", "unknown", "fire", "fire", "glitch",
+    "dragon", "fire", "water", "normal", "leaf", "fire", "normal", "dragon", "leaf",
+    "normal", "dragon", "normal", "dark", "fire", "leaf", "dark", "unknown", "unknown",
+    "leaf", "leaf", "dark", "dark", "dragon", "normal", "unknown", "dragon", "unknown",
+    "item", "leaf", "dark", "fire", "normal", "water", "fire", "leaf", "leaf",
+    
+    "fairy", "dark", "leaf", "dark", "normal", "dark", "normal", "normal", "item",
+    "metal", "leaf", "dark", "fighting", "item", "metal", "unknown", "leaf", "leaf",
+    "normal", "fighting", "fairy", "normal", "leaf", "leaf", "fighting", "water", "fighting",
+    "item", "normal", "item", "item", "leaf", "normal", "leaf", "metal", "leaf",
+    "water", "fire", "metal", "fire", "leaf", "item", "fighting", "fighting", "dragon",
+    "normal", "leaf", "unknown", "leaf", "leaf", "dragon", "dark", "leaf", "fighting",
+    "leaf", "fire", "normal", "electric", "unknown", "leaf", "fire", "leaf", "leaf",
+    "dark", "dragon", "fire", "normal", "normal", "dragon", "item", "item", "dark",
+    "dark", "water", "dragon", "leaf", "dragon", "fighting", "electric", "dark", "dark",
+    
+    "normal"
     };
     
     
@@ -74,6 +114,26 @@ public class Card
     'c', 'e', 'r', 'c', 'c', 'r', 'r', 'r', 'c',
     'c', 'c', 'c', 'c', 'r', 'c', 'c', 'c', 'c',
     'c', 'c', 'c', 'c', 'r', 'c', 'e', 'c', 'r',
+    
+    'c', 'c', 'c', 'l', 'r', 'e', 'e', 'c', 'l',
+    'c', 'c', 'c', 'r', 'l', 'r', 'c', 'r', 'c',
+    'e', 'e', 'c', 'i', 'r', 'c', 'c', 'r', 'c',
+    'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
+    'c', 'c', 'c', 'c', 'c', 'r', 'r', 'r', 'l',
+    'c', 'r', 'r', 'c', 'c', 'c', 'e', 'e', 'c',
+    'c', 'e', 'l', 'c', 'r', 'c', 'c', 'c', 'c',
+    'r', 'r', 'c', 'c', 'r', 'c', 'c', 'r', 'c',
+    'r', 'c', 'e', 'r', 'l', 'c', 'e', 'c', 'r',
+    
+    'l', 'c', 'r', 'c', 'c', 'c', 'l', 'c', 'c',
+    'r', 'e', 'c', 'c', 'e', 'c', 'r', 'c', 'c',
+    'r', 'c', 'r', 'l', 'c', 'c', 'r', 'r', 'c',
+    'e', 'c', 'r', 'l', 'c', 'r', 'c', 'c', 'c',
+    'c', 'r', 'r', 'r', 'r', 'r', 'c', 'c', 'r',
+    'e', 'c', 'r', 'c', 'c', 'c', 'c', 'r', 'c',
+    'c', 'c', 'r', 'c', 'r', 'c', 'c', 'r', 'c',
+    'c', 'r', 'e', 'r', 'c', 'c', 'r', 'c', 'c',
+    'c', 'r', 'l', 'c', 'c', 'c', 'c', 'c', 'c',
     
     'c'
     };
@@ -102,7 +162,27 @@ public class Card
     40, 50, 40, 50, 80, 50, 10, 50, 50,
     60, 60, 60, 50, 100, 60, 140, 50, 60,
     
-    40,
+    40, 60, 70, 200, 70, 130, 140, 70, 160,
+    50, 30, 60, 90, 90, 70, 50, 60, 50,
+    120, 40, 80, 120, 80, 60, 60, 90, 60,
+    50, 50, 60, 90, 60, 50, 60, 70, 60,
+    70, 60, 60, 50, 60, 80, 80, 50, 100,
+    90, 90, 70, 50, 50, 70, 130, 140, 60,
+    60, 150, 160, 50, 70, 50, 50, 30, 60,
+    80, 60, 60, 40, 140, 30, 40, 110, 60,
+    40, 60, 60, 80, 180, 50, 90, 60, 70,
+    
+    200, 60, 70, 60, 60, 60, 170, 50, -10,
+    90, 150, 50, 60, 40, 90, 70, 50, 60,
+    80, 80, 90, 170, 70, 70, 100, 90, 60,
+    40, 30, 30, 50, 50, 70, 60, 40, 70,
+    50, 80, 70, 60, 30, 30, 70, 70, 120,
+    150, 60, 70, 70, 60, 100, 50, 60, 70,
+    60, 60, 120, 60, 50, 60, 80, 100, 70,
+    50, 100, 120, 80, 40, 100, 30, 0, 60,
+    50, 100, 200, 60, 90, 70, 70, 50, 60,
+    
+    70,
     };
     
     
